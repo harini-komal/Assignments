9f21582fc017d26bafc822604b4d337bd57cd40f
Harini.R <miss.hariniravikumar@gmail.com>
2024-03-27 09:59:03 +0530

    my first push to git

---
+++ basicprogram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Hello World, This is my first C++ Programe";
+   cout<<endl; //to print in next in line
+  cout<<"Hope so my coding path will be worth of all my hardwork";
+  cout<<endl;
+ int x;//creating/declaraing the variable naming,x
+  x=5;//assigning/initiallization the value of x
+ cout<<x;//printing the value of x
+  cout<<endl;
+  int y=10;
+  cout<<y+x;
+  //updation of variable
+  
+  
+}
